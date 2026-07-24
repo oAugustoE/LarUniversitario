02eda2bacb239094cd0bdfef77cfb557deb259f9
oAugustoE <augustoemmribas@hotmail.com>
2026-07-24 18:07:15 -0300

    feat(database): User creation query and initial database query

---
+++ database/databasequeries.sql
@@ -0,0 +1,16 @@
+CREATE TYPE user_type AS ENUM ('student', 'owner', 'rental', 'republic');
+CREATE TYPE user_role AS ENUM ('admin', 'user');
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    user_type user_type NOT NULL DEFAULT 'student',
+    profile_picture VARCHAR(255),
+    institution VARCHAR(100),
+    description varchar(2000),
+    user_role ENUM('admin', 'user') NOT NULL DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
